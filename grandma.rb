1a5e21f6c8adb56f7ccb64020db96fa0712a0d48
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-12 20:00:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -25,9 +25,9 @@ def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
     return "HUH?! SPEAK UP, SONNY!"
    elsif phrase == "WHAT DID YOU EAT TODAY"
     return  "NO, NOT SINCE 1938!"
-  elsif phrase == "WHAT?"
+   else phrase == "WHAT?"
      return "NO, NOT SINCE 1938!"
-  else
+
    end
 
   end
