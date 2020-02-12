4e6c7b0a96bf3fc76fda68299508840302a08f29
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-12 20:01:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -21,7 +21,7 @@ def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
      return "I LOVE YOU TOO PUMPKIN!"
    elsif phrase == "Hi Nana, how are you?"
      return "HUH?! SPEAK UP, SONNY!"
-   elsif phrase == "HI"
+   elsif phrase == "Hi!"
     return "HUH?! SPEAK UP, SONNY!"
    elsif phrase == "WHAT DID YOU EAT TODAY"
     return  "NO, NOT SINCE 1938!"
